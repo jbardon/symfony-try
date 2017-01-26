878056bc20ab10a112ae83c85b5f60880f3b695e
Bardon Jeremy <jbardon.prestataire@voyages-sncf.com>
2017-01-26 14:13:31 +0100

    Upto 16/176

--- src/AppBundle/Controller/ChapterFiveteenController.php
+++ src/AppBundle/Controller/ChapterFiveteenController.php
@@ -7,6 +7,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
 
 class ChapterFiveteenController extends Controller
 {
@@ -18,14 +20,65 @@ class ChapterFiveteenController extends Controller
         return $this->render('chapter15/admin.html.twig');
     }
 
-    /*
-    public function numberSimpleAction()
+
+    /**
+     * @Route("/chapter15/denied")
+     */
+    public function deniedAction()
     {
+        $this->denyAccessUnlessGranted('ROLE_USER', null, 'Unable to access this page!');
+    }
+
+    /**
+     * @Route("/chapter15/annoted")
+     * @Security("has_role('ROLE_USER')")
+     */
+    public function annotedAction()
+    {
+        return $this->render('chapter15/admin.html.twig');
+    }
+
+    /**
+     * @Route("/chapter15/isauth")
+     */
+    public function authAction()
+    {
+        // IS_AUTHENTICATED_REMEMBERED, IS_AUTHENTICATED_ANONYMOUSLY
+        if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            throw $this->createAccessDeniedException();
+        }
+
         return $this->render(
-            'chapter5/number.html.twig',
-            array('luckyNumberList' => "1, 85, 21, 95")
+            'chapter15/message.html.twig',
+            array(
+                'message' => "authenticated"
+            )
         );
     }
-    */
 
+    /**
+     * @Route("/chapter15/user")
+     */
+    public function userAction()
+    {
+
+        // Toujours checker sinon $this->getUser() = null
+        if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $user = $this->getUser();
+
+        // the above is a shortcut for this
+        // $user = $this->get('security.token_storage')->getToken()->getUser()
+
+        dump($user);
+
+        return $this->render(
+            'chapter15/message.html.twig',
+            array(
+                'message' => $user
+            )
+        );
+    }
 }
